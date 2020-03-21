ac77437150c0a86fb4d5a8524e3bacdea0a0dd9d
gottamsnehamary <61041030+gottamsnehamary@users.noreply.github.com>
2020-03-22 00:06:32 +0530

    project-builder-blog-create-and-read-blog

--- src/dao/BlogDaoImpl.java
+++ src/dao/BlogDaoImpl.java
@@ -1,1 +1,70 @@
 package dao;
+
+import java.sql.Statement;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import model.Blog;
+import utility.ConnectionManager;
+
+public class BlogDaoImpl implements  BlogDaoInterface
+{
+
+	public void insertBlog(Blog blog) throws SQLException 
+	{
+		// TODO Auto-generated method stub
+		Connection con=ConnectionManager.getConnection();
+		
+		String sql="INSERT INTO BLOG(blogId,blogName ,blogDiscription,postedOn)VALUES(?,?,?,?)";
+		PreparedStatement st=null;
+		
+			st = con.prepareStatement(sql);
+			st.setLong(1,blog.getBlogId());
+			st.setString(2,blog.getBlogTitle());
+			st.setString(3,blog.getBlogDescription());
+			st.setDate(4,java.sql.Date.valueOf(blog.getPostedOn()));
+			st.executeUpdate();
+		
+			con.close();
+	}
+	
+	public List<Blog> selectAllBlogs() throws SQLException 
+	{
+		List<Blog> list=new ArrayList<Blog>();
+		// TODO Auto-generated method stub
+		Connection con=ConnectionManager.getConnection();
+       Statement stmt=  con.createStatement();
+		ResultSet rs =stmt.executeQuery("select * from BLOG ");
+		
+		while(rs.next())
+		{
+			long id=rs.getLong(1);
+			String name=rs.getString(2);
+			String description=rs.getString(3);
+			Date d=rs.getDate(4);
+		
+			Blog blog=new Blog();
+			blog.setBlogId(id);
+			blog.setBlogTitle(name);
+			 blog.setBlogDescription(description);
+
+			 LocalDate postedOn = rs.getDate("postedOn").toLocalDate();
+			 blog.setPostedOn(postedOn);
+			list.add(blog);	
+			
+		}
+		
+	
+		return list;
+	}
+	
+	
+}
